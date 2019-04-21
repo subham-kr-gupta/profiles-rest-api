ceb84d259a36b026d9e77c1d07050f490a71bbce
Subham Kumar <kumarsubham266.sk@gmail.com>
2019-04-21 20:20:55 +0530

    Created our first API View

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.urls import include
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('profiles_api.urls'))
+    path('hello-view/', views.HelloApiView.as_view()),
 ]
